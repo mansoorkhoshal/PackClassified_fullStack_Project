79c1c08c807c8194d8158a55ee69daec213a2392
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2026-01-20 15:35:38 +0500

    feat: adding the multer for image to store in database

--- backend/Middlware/uploadImage.js
+++ backend/Middlware/uploadImage.js
@@ -1,16 +1,15 @@
 const multer = require("multer");
-const path=require('path')
+const path = require("path");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
   },
-  filename:function(req,file,cb){
-    const newFileName= Date.now() + path.extname(file.originalname)
-    cb(null,newFileName)
-  }
+  filename: function (req, file, cb) {
+    const newFileName = Date.now() + path.extname(file.originalname);
+    cb(null, newFileName);
+  },
 });
 
+const uploads = multer({ storage });
 
-const uploads=multer({storage})
-
-module.exports=uploads
+module.exports = uploads;
